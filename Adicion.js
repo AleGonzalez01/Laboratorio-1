8ce741e1edbcdff41c310d6be34129013cb5589a
mdmlb <53907000+mdmlb@users.noreply.github.com>
2019-09-13 09:25:17 -0500

    agrege metodos la clase adicion

--- Adicion.js
+++ Adicion.js
@@ -1,7 +1,43 @@
 let precioAdición;
+let posY;
+let posX;
 class Adicion{
 constructor(precioAdición){
-    this.precioAdición=precioAdición;
+    this.precioAdición = precioAdición;
+    this.arregloBotonAdicion =[];
+    this.posX=posX;
+    this.posY=posY;
+    this.posYmas=0;
+    this.oton=false;
+    this.enfocado=false;
 
+    for(let i=0; i<4; i++){
+        this.arregloBotonAdicion.push(614,282+this.posYmas);
+        this.posYmas+=43;
+    }
 }
+
+pintarBotonAdicion(){
+    if(this.enfocado == true){
+        for(let i=0; i<this.arregloBotonAdicion.length; i++){
+            fill(0);
+            ellipse(615,282,12,12);
+        }
+    }
+    
+}
+/*mouseY >= 277 && mouseY <= 277+10 && mouseX >= 610 && mouseX <= 610+10 */
+botonFocus(mousex,mousey){
+    for(let i=0; i<this.arregloBotonAdicion.length;i++){
+        if(mousex >= 277 && mousex <= 277+10 &&
+            mousey >= 610 && mousey <= 610+10){
+                this.arregloBotonAdicion[i].enfocado = false;
+            }
+        if(mousex <= 277 && mousex >= 277+10 &&
+            mousey <= 610 && mousey >= 610+10){
+                this.arregloBotonAdicion[i].enfocado = true;
+            }
+        }
+    }
+
 }
